638090553e73b24ed139a57e0967ea999dd1af0d
Doug Ransom <doug.ransom@gmail.com>
2022-12-31 12:19:05 -0800

    checkpoint

--- tests/test_grammar_control.py
+++ tests/test_grammar_control.py
@@ -2,37 +2,15 @@
 from pathlib import Path
 import pytest
 
-#IMPORTANT
-#patching of environment to point to correct natlink.ini needs to happen before loading nay natlink related code
-
-natlink_folder=str(Path(__file__).parent / "unimacro_test_natlink_config.natlink")
-print(f"{__file__}  natlink folder {natlink_folder}")
-pytest.MonkeyPatch().setenv("NATLINK_USERDIR",natlink_folder)
-
 import natlink
 from unimacro._control import UtilGrammar
 from unimacro import natlinkutilsbj as natbj
-#from natlinkcore import natlinkstatus
-#status = natlinkstatus.NatlinkStatus
 
 thisDir = Path(__file__).parent
-#@pytest.fixture 
-#def status():
-#    from natlinkcore import natlinkstatus
-#    status = natlinkstatus.NatlinkStatus
-#    return status
 
 def do_nothing(*args, **kwargs):
     return None
-
-def mock_unimacro_user_dir(tmp_dir,_self):
-
-    mock_folder= tmp_dir / "mock_unimacro_userdir"
-    print(f"Mock unimacro folder {mock_folder} in {__file__}")   #just for understanding remove eventually
-    if not mock_folder.is_dir():
-        mock_folder.mkdir()
-    return str(mock_folder)
-
+ 
 class GramOn(natbj.IniGrammar):
     """simple grammar which is initially on
     """
